3e63b5d9d6fc48454e1ee69756cad5f4954e401f
STARG-LEE <67219412+STARG-LEE@users.noreply.github.com>
2024-10-29 09:31:08 +0900

    Update kwsentindex.py

--- kwsentindex.py
+++ kwsentindex.py
@@ -82,7 +82,7 @@ sentiment_trend1['6개월 이동평균'] = sentiment_trend1['index'].rolling(win
 
 # 그래프 그리기
 fig = plt.figure(figsize=(12, 6))
-plt.plot(sentiment_trend1['year_month'], sentiment_trend1['index'], marker='o', color='g', label='감성지수', fontproperties = fontprop)
+plt.plot(sentiment_trend1['year_month'], sentiment_trend1['index'], marker='o', color='g', label='감성지수')
 plt.plot(sentiment_trend1['year_month'], sentiment_trend1['6개월 이동평균'], color='blue', linestyle='-', linewidth=2,
          label='6개월 이동평균', fontproperties = fontprop)
 plt.axhline(y=0.5, color='r', linestyle='--', fontproperties = fontprop)
